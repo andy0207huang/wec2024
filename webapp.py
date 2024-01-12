98aac09c431c8771b597ec7e5473a07468177e49
ahuang95 <andy0207huang@gmail.com>
2024-01-12 18:07:58 -0500

    Select the Disaster Name - webapp

---
+++ webapp.py
@@ -0,0 +1,40 @@
+# ---WEBAPP---
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+import altair as alt
+import matplotlib.pyplot as plt
+import numpy as np
+
+st.set_page_config(
+    page_title="WEC 2024 - Natural Disaster",
+    page_icon=":bar_chart:",
+    layout="centered"
+)
+
+dfcsv = pd.read_csv("MOCK_DATA.csv")
+
+# DASHBOARD
+st.title(":bar_chart: Natural Disaster Webapp")
+st.markdown("##")
+
+
+# ========
+# # FILTER
+st.sidebar.header("Please Filter Here:")
+
+disasterName = st.sidebar.multiselect(
+    "Select the Disaster Name:",
+    options=dfcsv["Name"].unique(),
+    default=dfcsv["Name"].unique()
+)
+
+df_selection = dfcsv.query(
+    "Name == @disasterName"
+)
+
+
+# # --dynamic--
+# # TABLE
+st.header("Scraped Data")
+st.dataframe(df_selection)
