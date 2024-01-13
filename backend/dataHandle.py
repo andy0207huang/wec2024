5b371b82b7c2b46d20f17a2cd95fa106f467b14c
PikaKight <tmmarcus73@gmail.com>
2024-01-12 23:20:16 -0500

    added comments

--- backend/dataHandle.py
+++ backend/dataHandle.py
@@ -15,28 +15,39 @@ def editData(path: str, data: pd.DataFrame, value: str, name: str, date: str) ->
     data.to_csv(path, index=False)
 
 def addRow(path: str, data: pd.DataFrame, row: dict) -> None:
+    # Gets the country from lat and long
     row['Country'] = convertToCountry(row['lat'], row['long'])
     
+    # Converts the date to month/day/year
     row['date'] = pd.to_datetime(row['date']).strftime("%#m/%#d/%Y")
 
+    # Adds the row
     data.loc[len(data.index)] = row
 
+    # Save CSV
     data.to_csv(path, index=False)
 
 
 
 def addCountry(path: str, data: pd.DataFrame, col: str) -> None:
 
+    # Creates a new column called country and populate it with Na
     data[col] = "Na"
 
+    # Loop through all the rows
     for i in range(len(data.index)):
 
+        # Get latitude and longitude
         lat = data.iloc[i]['lat']
         long = data.iloc[i]['long']
 
+        # Gets the country
         country = convertToCountry(lat, long)
+
+        # Updates the Country column of the row
         data.loc[i, [col]] = country
 
+    # Save csv
     data.to_csv(path, index=False)
 
 if __name__ == "__main__":
