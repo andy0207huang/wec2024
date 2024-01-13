e57766392fda93bd436e072e2cbcffaaa944b097
PikaKight <tmmarcus73@gmail.com>
2024-01-12 23:21:55 -0500

    added comments

--- backend/locations.py
+++ backend/locations.py
@@ -2,14 +2,19 @@ from geopy.geocoders import Nominatim
 
 
 def convertToCountry(Lat: float,Lon: float) -> str:
+    
+    # Init Nominatim object
     geolocator = Nominatim(user_agent="WEC2024")
-
+        
     try:
+        # Gets the geolocation data
         location = geolocator.reverse(f"{Lat},{Lon}", language="en")
 
+        # Gets the country
         country = location.raw["address"]["country"]
     
     except Exception as e:
+        # If it can't find the country, set as Na
         country = "Na"
 
     return country
